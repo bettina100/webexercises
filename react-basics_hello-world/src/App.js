442bac62e88f72c8d80f85bd986f474e5d24761f
Norbert Wowy <norbert.wowy@gmail.com>
2024-01-25 11:13:34 +0100

    Add challenge react basics

---
+++ react-basics_hello-world/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <h1>Hallo world</h1>;
+}
+
+function helloWorld() {
+  return <h1>Hallo world</h1>;
+}
